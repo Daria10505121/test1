e8522dca81df0bc557dcc62f578999af670fc5dc
Daria_Vocuga <dvolcuga@gmail.com>
2024-06-19 16:05:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <p>Привет, мир!</p>
+<?php
+
+$x = 2;
+$y = 3;
+$z = $x+=$y;
+echo $z; 
+$name = "Daria";
+$surname = "Volcuga";
+echo $name . $surname;
+echo $name , $surname;
+echo "My name:" . $name;
+echo "My name:\n" . $name, " " . $surname;
+// kfgsjkh 
+
+?>
+</body>
+</html>
